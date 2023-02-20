42666abbbfad3e690530e388ed86da2ca52568c6
Павел <chernovpp@gmail.com>
2023-02-20 21:50:49 +0700

    ДЗ Dj_3.1 Ok!

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from users.models import *
 
 
+"""Сериализатор для локаций"""
+class LocationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Location
+        fields = '__all__'
+
+
 """Сериализатор для создания пользователя"""
 class UserCreateSerializer(serializers.ModelSerializer):
     location = serializers.SlugRelatedField(
@@ -11,23 +19,50 @@ class UserCreateSerializer(serializers.ModelSerializer):
         slug_field='name'
     )
 
+    class Meta:
+        model = User
+        fields = '__all__'
+
     def is_valid(self, *, raise_exception=False):
         self._locations = self.initial_data.pop("location", [])
         return super().is_valid(raise_exception=raise_exception)
 
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
+
         for loc_name in self._locations:
             location, _ = Location.objects.get_or_create(name=loc_name)
             user.location.add(location)
         return user
 
+
+"""Сериализатор для обновления пользователя"""
+class UserUpdateSerializer(serializers.ModelSerializer):
+    location = serializers.SlugRelatedField(
+        required=False,
+        queryset=Location.objects.all(),
+        many=True,
+        slug_field='name'
+    )
+
     class Meta:
         model = User
         fields = '__all__'
 
+    def is_valid(self, *, raise_exception=False):
+        self._locations = self.initial_data.pop("location", [])
+        return super().is_valid(raise_exception=raise_exception)
 
-"""Сериализатор для пользователя при выводе одного объявления"""
+    def save(self):
+        user = super().save()
+
+        for loc_name in self._locations:
+            location, _ = Location.objects.get_or_create(name=loc_name)
+            user.location.add(location)
+        return user
+
+
+"""Сериализатор для пользователя при выводе части его полей в одном объявлении"""
 class UserAdSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -35,9 +70,24 @@ class UserAdSerializer(serializers.ModelSerializer):
         fields = ['first_name', 'last_name', 'username']
 
 
-"""Сериализатор для локаций"""
-class LocationSerializer(serializers.ModelSerializer):
+"""Сериализатор для вывода пользователей списком или по одному со всеми полями"""
+class UsersSerializer(serializers.ModelSerializer):
+    location = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
 
     class Meta:
-        model = Location
+        model = User
         fields = '__all__'
+
+
+"""Сериализатор для удаления пользователя"""
+class UsersDestroySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id']
+
+
--- users/views.py
+++ users/views.py
@@ -1,68 +1,75 @@
-import json
-
-from django.core.paginator import Paginator
-from django.http import JsonResponse
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
-
-from dz_dj_1.settings import TOTAL_ON_PAGE
-from users.models import User, Location
-from users.serializers import UserCreateSerializer, LocationSerializer
-
-"""
-Список пользователей с пагинацией по 10 на странице.
-"""
-class UserListView(ListView):
-    model = User
-
-    def get(self, request, *args, **kwargs):
-        super().get(request, *args, **kwargs)
-        # users = self.object_list.all()
-        self.object_list = self.object_list.order_by('username')
-        paginator = Paginator(self.object_list, TOTAL_ON_PAGE)
-        page = request.GET.get('page')
-        obj = paginator.get_page(page)
-        response = {}
-        items_list = [
-            {'id': user.pk,
-             'first_name': user.first_name,
-             'last_name': user.last_name,
-             'username': user.username,
-             'password': user.password,
-             'role': user.role,
-             'age': user.age,
-             'locations': list(map(str, user.location.all())),
-             'total_ads': user.ads.filter(is_published=True).count()} for user in obj]
-        response['items'] = items_list
-        response['total'] = self.object_list.count()
-        response['num_pages'] = paginator.num_pages
-
-        return JsonResponse(response, safe=False, json_dumps_params={"ensure_ascii": False})
+from users.serializers import *
 
 
-"""
-Вывод указанного пользователя.
-"""
-class UserDetailView(DetailView):
-    # queryset = User.objects.all()
-    model = User
+""" Список пользователей. """
+class UserListView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UsersSerializer
 
-    def get(self, request, *args, **kwargs):
-        user = self.get_object()
+# """
+# Список пользователей с пагинацией по 10 на странице.
+# """
+# class UserListView(ListView):
+#     model = User
+#
+#     def get(self, request, *args, **kwargs):
+#         super().get(request, *args, **kwargs)
+#         # users = self.object_list.all()
+#         self.object_list = self.object_list.order_by('username')
+#         paginator = Paginator(self.object_list, TOTAL_ON_PAGE)
+#         page = request.GET.get('page')
+#         obj = paginator.get_page(page)
+#         response = {}
+#         items_list = [
+#             {'id': user.pk,
+#              'first_name': user.first_name,
+#              'last_name': user.last_name,
+#              'username': user.username,
+#              'password': user.password,
+#              'role': user.role,
+#              'age': user.age,
+#              'locations': list(map(str, user.location.all())),
+#              'total_ads': user.ads.filter(is_published=True).count()} for user in obj]
+#         response['items'] = items_list
+#         response['total'] = self.object_list.count()
+#         response['num_pages'] = paginator.num_pages
+#
+#         return JsonResponse(response, safe=False, json_dumps_params={"ensure_ascii": False})
 
-        return JsonResponse({'id': user.pk,
-                             'first_name': user.first_name,
-                             'last_name': user.last_name,
-                             'username': user.username,
-                             'password': user.password,
-                             'role': user.role,
-                             'age': user.age,
-                             'locations': list(map(str, user.location.all())),
-                             'total_ads': user.ads.filter(is_published=True).count()
-                             }, safe=False, json_dumps_params={"ensure_ascii": False})
+
+""" Информация об одном пользователе. """
+class UserDetailView(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UsersSerializer
+
+# """
+# Вывод указанного пользователя.
+# """
+# class UserDetailView(DetailView):
+#     # queryset = User.objects.all()
+#     model = User
+#
+#     def get(self, request, *args, **kwargs):
+#         user = self.get_object()
+#
+#         return JsonResponse({'id': user.pk,
+#                              'first_name': user.first_name,
+#                              'last_name': user.last_name,
+#                              'username': user.username,
+#                              'password': user.password,
+#                              'role': user.role,
+#                              'age': user.age,
+#                              'locations': list(map(str, user.location.all())),
+#                              'total_ads': user.ads.filter(is_published=True).count()
+#                              }, safe=False, json_dumps_params={"ensure_ascii": False})
+
+
+""" Создание пользователя. """
+class UserCreateView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
 
 # """
 # Создание пользователя.
@@ -95,62 +102,63 @@ class UserDetailView(DetailView):
 #                              'age': user.age,
 #                              'locations': list(map(str, user.location.all())),
 #                              }, safe=False, json_dumps_params={"ensure_ascii": False})
-"""
-Создание пользователя.
-"""
-class UserCreateView(CreateAPIView):
+
+""" Обновление данных указанного пользователя. """
+class UserUpdateView(UpdateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserCreateSerializer
+    serializer_class = UserUpdateSerializer
 
+#
+# """ Обновление данных указанного пользователя. """
+# @method_decorator(csrf_exempt, name='dispatch')
+# class UserUpdateView(UpdateView):
+#     model = User
+#     fields = ['username']
+#
+#     def patch(self, request, *args, **kwargs):
+#         super().post(request, *args, **kwargs)
+#         data = json.loads(request.body)
+#         if 'first_name' in data:
+#             self.object.first_name = data['first_name']
+#         if 'last_name' in data:
+#             self.object.last_name = data['last_name']
+#         if 'role' in data:
+#             self.object.role = data['role']
+#         if 'age' in data:
+#             self.object.age = data['age']
+#         if 'locations' in data:
+#             for loc_name in data['locations']:
+#                 loc, _ = Location.objects.get_or_create(name=loc_name)
+#                 self.object.location.add(loc)
+#         self.object.save()
+#
+#         return JsonResponse({'id': self.object.pk,
+#                              'first_name': self.object.first_name,
+#                              'last_name': self.object.last_name,
+#                              'username': self.object.username,
+#                              'password': self.object.password,
+#                              'role': self.object.role,
+#                              'age': self.object.age,
+#                              'locations': list(map(str, self.object.location.all())),
+#                              'total_ads': self.object.ads.filter(is_published=True).count()
+#                              }, safe=False, json_dumps_params={"ensure_ascii": False})
 
-"""
-Обновление данных указанного пользователя.
-"""
-@method_decorator(csrf_exempt, name='dispatch')
-class UserUpdateView(UpdateView):
-    model = User
-    fields = ['username']
+"""Удаление указанного пользователя."""
+class UserDeleteView(DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UsersDestroySerializer
 
-    def patch(self, request, *args, **kwargs):
-        super().post(request, *args, **kwargs)
-        data = json.loads(request.body)
-        if 'first_name' in data:
-            self.object.first_name = data['first_name']
-        if 'last_name' in data:
-            self.object.last_name = data['last_name']
-        if 'role' in data:
-            self.object.role = data['role']
-        if 'age' in data:
-            self.object.age = data['age']
-        if 'locations' in data:
-            for loc_name in data['locations']:
-                loc, _ = Location.objects.get_or_create(name=loc_name)
-                self.object.location.add(loc)
-        self.object.save()
-
-        return JsonResponse({'id': self.object.pk,
-                             'first_name': self.object.first_name,
-                             'last_name': self.object.last_name,
-                             'username': self.object.username,
-                             'password': self.object.password,
-                             'role': self.object.role,
-                             'age': self.object.age,
-                             'locations': list(map(str, self.object.location.all())),
-                             'total_ads': self.object.ads.filter(is_published=True).count()
-                             }, safe=False, json_dumps_params={"ensure_ascii": False})
-
-
-"""
-Удаление указанного пользователя.
-"""
-@method_decorator(csrf_exempt, name='dispatch')
-class UserDeleteView(DeleteView):
-    model = User
-    success_url = "/"
-
-    def delete(self, request, *args, **kwargs):
-        super().delete(request, *args, **kwargs)
-        return JsonResponse({'status': 'ok'}, status=204)
+# """
+# Удаление указанного пользователя.
+# """
+# @method_decorator(csrf_exempt, name='dispatch')
+# class UserDeleteView(DeleteView):
+#     model = User
+#     success_url = "/"
+#
+#     def delete(self, request, *args, **kwargs):
+#         super().delete(request, *args, **kwargs)
+#         return JsonResponse({'status': 'ok'}, status=204)
 
 
 """
